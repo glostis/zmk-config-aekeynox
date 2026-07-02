e492e74e39552ee604f9e9f47897bccd5f3bb7a2
Guillaume Lostis <guillaume@lostis.com>
2026-07-02 17:38:30 +0200

    tweak selenium to my liking
    - Ergo-L, HRM and VimNav
    - Backspace on right pinky in VimNav
    - HRM ctrl or shift-tab on left middle finger in VimNav
    - Base behavior for left 2 thumbs in symbols layer
    - Base behavior for middle left thumb in numrow layer
    - Add compose key

--- include/aekeynox/settings.h
+++ include/aekeynox/settings.h
@@ -14,7 +14,7 @@
 // #define KB_LAYOUT_BEPO
 // #define KB_LAYOUT_BEPOLAR
 // #define KB_LAYOUT_DVORAK
-// #define KB_LAYOUT_ERGOL
+#define KB_LAYOUT_ERGOL
 // #define KB_LAYOUT_ERGLACE
 // #define KB_LAYOUT_QWERTY_INTL
 // #define KB_LAYOUT_QWERTY_LAFAYETTE
@@ -64,7 +64,7 @@
 
 // #define HT_NONE
 // #define HT_THUMB_TAPS
-// #define HT_HOME_ROW_MODS  // (default behavior)
+#define HT_HOME_ROW_MODS  // (default behavior)
 // #define HT_TWO_THUMB_KEYS
 
 // Timing is key! Keep the default value if unsure.
@@ -102,7 +102,7 @@
 //  + Escape under the left thumb (direct access)
 // Highly recommended for Vim users, obviously. :-)
 
-// #define VIM_NAVIGATION
+#define VIM_NAVIGATION
 
 // [Experimental]
 // Uncomment the following line to enable the "mod-hold behavior" on the left
